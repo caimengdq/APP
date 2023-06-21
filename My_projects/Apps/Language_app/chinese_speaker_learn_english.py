e9a26fbedacb6f9fdead706e54bc77ddfdba3e44
caimengdq <caimengdq@gmail.com>
2023-06-21 17:01:01 +0200

    adding prompt

--- My_projects/Apps/Language_app/chinese_speaker_learn_english.py
+++ My_projects/Apps/Language_app/chinese_speaker_learn_english.py
@@ -8,6 +8,8 @@ from keys import *
 
 # higher temperature for higher randomness
 os.environ["OPENAI_API_KEY"] = key_openai
+
+
 # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.1)
 
 # use loader to load full transcript as a query database
@@ -15,8 +17,12 @@ os.environ["OPENAI_API_KEY"] = key_openai
 
 def summarise_from_episode(episode):
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.1)
-    prompt = PromptTemplate.from_template("Select 10 commonly used phrases from the show Modern Family {episode}, "
-                                          "provide translation in Chinese, also explain the context in Chinese")
+    prompt_1 = "Select 10 commonly used phrases from the show Modern Family {episode}, " \
+               "provide translation in Chinese, also explain the context in Chinese"
+    prompt_2 = "You are trying to help someone to improve their English level, conduct a lecture based on {episode} " \
+               "of Modern Family for a beginner Chinese speaker " \
+               "learning English, providing translations and explanations in Chinese "
+    prompt = PromptTemplate.from_template(prompt_2)
 
     chain = LLMChain(llm=llm, prompt=prompt)
 
@@ -24,7 +30,3 @@ def summarise_from_episode(episode):
 
 
 print(summarise_from_episode("Season 1 Episode 5"))
-
-
-
-
